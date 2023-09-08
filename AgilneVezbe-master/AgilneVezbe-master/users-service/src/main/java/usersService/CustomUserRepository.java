754f0046276a019b205435dd2adc873809ebbdbe
andreailic <andreailic45@gmail.com>
2023-09-08 18:49:21 +0200

    final version

--- AgilneVezbe-master/AgilneVezbe-master/users-service/src/main/java/usersService/CustomUserRepository.java
+++
@@ -1,9 +0,0 @@
-package usersService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import usersService.model.CustomUser;
-
-public interface CustomUserRepository extends JpaRepository<CustomUser, Long> {
-
-}
